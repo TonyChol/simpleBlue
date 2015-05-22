0c4034d2d5b66c5bc5fb041ceeb53a9560c3ca13
TonyChol <tonycai1992@gmail.com>
2015-05-22 16:33:45 +0800

    Theme header done

---
+++ Guardfile
@@ -0,0 +1,11 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard 'livereload' do
+  watch('index.hbs')
+  watch('default.hbs')
+  watch(%r{assets/css/.+\.(css)})
+  watch(%r{assets/js/.+\.(js)})
+end
+
+guard :compass
--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/stylesheets"
+css_dir = "assets/css"
 sass_dir = "assets/sass"
 images_dir = "assets/images"
 javascripts_dir = "assets/js"
