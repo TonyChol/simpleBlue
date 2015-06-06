c38f81def513fdbe4a3c5879db3f23aa5497c184
TonyChol <tonycai1992@gmail.com>
2015-06-06 23:50:58 +0800

    Update blog style details

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,6 @@ module.exports = function(grunt) {
         },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-compass');
